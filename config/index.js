f2bbb529d5e92ee455e6d2042208e667756683ad
JianJun.Xie <v-xiejj05@vanke.com>
2018-12-21 17:35:59 +0800

    change umd

--- config/index.js
+++ config/index.js
@@ -1,5 +1,6 @@
 import cjs from './rollup.config.cjs';
 import iife from './rollup.config.iife';
 import dev from './rollup.config.dev';
+import prod from './rollup.config.prod';
 
-export default { cjs, iife, dev };
+export default { cjs, iife, dev, prod };
